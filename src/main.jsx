6d59a29b037d90d7eda3f04fbf3d5fa2ba3ae032
agrabio134 <agrabioharvey@gmail.com>
2025-12-30 20:45:18 +0800

    fix build

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'   // ← Remove the .jsx extension
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 )
